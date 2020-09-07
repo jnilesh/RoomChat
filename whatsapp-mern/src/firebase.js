efc76d9e34a611adfc471080ee5de995e7e0b47b
Nilesh Jain <njain.we@gmail.com>
2020-09-07 22:14:31 +0530

    first

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2EF3n2yEUaojxeH4mg6kPobresU0qL_k",
+    authDomain: "roomchat-mern.firebaseapp.com",
+    databaseURL: "https://roomchat-mern.firebaseio.com",
+    projectId: "roomchat-mern",
+    storageBucket: "roomchat-mern.appspot.com",
+    messagingSenderId: "52122522331",
+    appId: "1:52122522331:web:eacf94c7f6d85b65bdb2fe"
+  };
